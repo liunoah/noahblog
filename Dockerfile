ce53078440560e336f66007da3a9bd9900da98cb
liunoah <89234625+liunoah@users.noreply.github.com>
2023-06-02 19:12:33 +0800

    add build

--- Dockerfile
+++ Dockerfile
@@ -1,22 +1,15 @@
 FROM node:16.20.0
 
-# 创建一个工作目录
+RUN npm install -g expo-cli && \
+npm install -g serve@14.2.0
+
 WORKDIR /app
 
-# 复制当前目录下的代码到工作目录
-COPY . .
-
-# 安装 expo-cli 和 serve
-RUN npm install -g expo-cli serve
-
-# 安装应用程序依赖
+COPY package.json .
 RUN npm install
 
-# 构建 Expo Web 应用程序
-RUN npx expo export:web --output-dir web-build
+COPY . .
 
-# 暴露 80 端口
-EXPOSE 80
+RUN npm run build
 
-# 启动静态文件服务器
-CMD ["npx", "serve", "web-build", "--single", "-l", "80"]
+CMD ["npx", "serve", "web-build", "--single"]
--- build.sh
+++ build.sh
@@ -1,2 +1,31 @@
-# build imags
-docker build -t noahblog .
+#!/bin/bash
+
+# Docker 镜像名称
+IMAGE_NAME="noahblog"
+
+# Docker 容器名称
+CONTAINER_NAME="noahblog"
+
+# 构建 Docker 镜像 "$(dirname "$0")"为当前路径
+docker build -t $IMAGE_NAME "$(dirname "$0")"
+
+# 检查容器是否正在运行
+if [ "$(docker ps -q -f name=$CONTAINER_NAME)" ]; then
+    # 关闭正在运行的容器
+    docker stop $CONTAINER_NAME
+fi
+
+# 检查容器是否存在
+if [ "$(docker ps -aq -f name=$CONTAINER_NAME)" ]; then
+    # 删除容器
+    docker rm $CONTAINER_NAME
+fi
+
+# 启动新的容器 外部端口3000
+docker run -d --name $CONTAINER_NAME -p 3000:3000 $IMAGE_NAME
+
+# 删除所有未被使用的镜像、容器和数据卷
+docker image prune
+
+# 查看运行容器
+docker logs $CONTAINER_NAME
